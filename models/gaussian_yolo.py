c5b02287fcd5a080a3cf0e963e480b1c5bee8c05
juanthomasw <104194668+juanthomasw@users.noreply.github.com>
2023-11-27 14:44:13 +0700

    Update gaussian_yolo.py

--- models/gaussian_yolo.py
+++ models/gaussian_yolo.py
@@ -20,7 +20,7 @@ except ImportError:
     thop = None
 
 
-class Detect(nn.Module): # Unchanged (Can't be used for Gaussian Modeling)
+class Detect(nn.Module):
     stride = None  # strides computed during build
     export = False  # onnx export
     end2end = False
@@ -29,37 +29,43 @@ class Detect(nn.Module): # Unchanged (Can't be used for Gaussian Modeling)
 
     def __init__(self, nc=80, anchors=(), ch=()):  # detection layer
         super(Detect, self).__init__()
-        self.nc = nc  # number of classes
-        self.no = nc + 5  # number of outputs per anchor
-        self.nl = len(anchors)  # number of detection layers
-        self.na = len(anchors[0]) // 2  # number of anchors
-        self.grid = [torch.zeros(1)] * self.nl  # init grid
-        a = torch.tensor(anchors).float().view(self.nl, -1, 2)
-        self.register_buffer('anchors', a)  # shape(nl,na,2)
-        self.register_buffer('anchor_grid', a.clone().view(self.nl, 1, -1, 1, 1, 2))  # shape(nl,1,na,1,1,2)
-        self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
+        self.nc = nc  # number of classes -> nc=1
+        self.no = nc + 9  # number of outputs per anchor with xywh uncertainties
+        self.nl = len(anchors)  # number of detection layers -> nl = 3
+        self.na = len(anchors[0]) // 2  # number of anchors -> na = 3
+        self.grid = [torch.zeros(1)] * self.nl  # init grid - grid list 
+        a = torch.tensor(anchors).float().view(self.nl, -1, 2) # anchors in shape (n1,-1,2) -> (3,3,2)
+        self.register_buffer('anchors', a)  # shape(nl,na,2) -> (3,3,2)
+        self.register_buffer('anchor_grid', a.clone().view(self.nl, 1, -1, 1, 1, 2))  # shape(nl,1,na,1,1,2) -> (3,1,3,1,1,2)
+        self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv (x, no*3, 1)
 
     def forward(self, x):
         # x = x.copy()  # for profiling
         z = []  # inference output
         self.training |= self.export
         for i in range(self.nl):
-            x[i] = self.m[i](x[i])  # conv
-            bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
-            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+            x[i] = self.m[i](x[i])  # conv -> shape (bs, no*3, ny, nx)
+            bs, _, ny, nx = x[i].shape  # x(bs,no*3,ny,nx)
+            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous() # x(bs,no*3,ny,nx) to x(bs,3,ny,nx,no)
 
             if not self.training:  # inference
                 if self.grid[i].shape[2:4] != x[i].shape[2:4]:
                     self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
+                
                 y = x[i].sigmoid()
                 if not torch.onnx.is_in_onnx_export():
-                    y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
-                    y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+                    y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy compensation term
+                    y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh compensation term
+                    
                 else:
-                    xy, wh, conf = y.split((2, 2, self.nc + 1), 4)  # y.tensor_split((2, 4, 5), 4)  # torch 1.8.0
+                    xy = y[..., 0:2]
+                    wh = y[..., 2:4]
+                    others = y[..., 4:]
+                    
                     xy = xy * (2. * self.stride[i]) + (self.stride[i] * (self.grid[i] - 0.5))  # new xy
                     wh = wh ** 2 * (4 * self.anchor_grid[i].data)  # new wh
-                    y = torch.cat((xy, wh, conf), 4)
+                    y = torch.cat((xy, wh, others), 4)
+                    
                 z.append(y.view(bs, -1, self.no))
 
         if self.training:
@@ -83,9 +89,9 @@ class Detect(nn.Module): # Unchanged (Can't be used for Gaussian Modeling)
 
     def convert(self, z):
         z = torch.cat(z, 1)
-        box = z[:, :, :4]
-        conf = z[:, :, 4:5]
-        score = z[:, :, 5:]
+        box = z[:, :, :8]
+        conf = z[:, :, 8:9]
+        score = z[:, :, 9:]
         score *= conf
         convert_matrix = torch.tensor([[1, 0, 1, 0], [0, 1, 0, 1], [-0.5, 0, 0.5, 0], [0, -0.5, 0, 0.5]],
                                            dtype=torch.float32,
@@ -202,9 +208,9 @@ class IDetect(nn.Module):
 
     def convert(self, z):
         z = torch.cat(z, 1)
-        box = z[:, :, :4]
-        conf = z[:, :, 4:5]
-        score = z[:, :, 5:]
+        box = z[:, :, :8]
+        conf = z[:, :, 8:9]
+        score = z[:, :, 9:]
         score *= conf
         convert_matrix = torch.tensor([[1, 0, 1, 0], [0, 1, 0, 1], [-0.5, 0, 0.5, 0], [0, -0.5, 0, 0.5]],
                                            dtype=torch.float32,
