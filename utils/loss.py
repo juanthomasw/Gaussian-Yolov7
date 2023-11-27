1f9dfe6262077417ef37c92f5fed80d5a8d3fd32
juanthomasw <104194668+juanthomasw@users.noreply.github.com>
2023-11-27 17:27:37 +0700

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -429,7 +429,7 @@ class ComputeLoss:
         # Define criteria
         BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['cls_pw']], device=device))
         BCEobj = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['obj_pw']], device=device))
-        NLLbox = nn.GaussianNLLLoss(device=device)
+        NLLbox = nn.GaussianNLLLoss()
 
         # Class label smoothing https://arxiv.org/pdf/1902.04103.pdf eqn 3
         self.cp, self.cn = smooth_BCE(eps=h.get('label_smoothing', 0.0))  # positive, negative BCE targets
@@ -571,7 +571,7 @@ class ComputeLossOTA:
         # Define criteria
         BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['cls_pw']], device=device))
         BCEobj = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['obj_pw']], device=device))
-        NLLbox = nn.GaussianNLLLoss(device=device)
+        NLLbox = nn.GaussianNLLLoss()
 
         # Class label smoothing https://arxiv.org/pdf/1902.04103.pdf eqn 3
         self.cp, self.cn = smooth_BCE(eps=h.get('label_smoothing', 0.0))  # positive, negative BCE targets
